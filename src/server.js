94eee348dac5b4dd0f1aac9f29196dfd474fa291
Denis Miranda <denis.mj@outlook.com>
2020-01-07 16:37:56 -0600

    FIX: CORS support

--- src/server.js
+++ src/server.js
@@ -1,21 +1,18 @@
 import express from 'express'
-import {router} from './routes/routes'
-var app = express()
-var port = 8000
+var cors = require('cors')
 
+var app = express()
 //Middleware to allow Access-Control-Origin on all HTTP requests
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-});
+app.use(cors())
+app.options('*', cors());
+import { router } from './routes/routes'
+var port = 8000
 
 app.use(router)
 app.listen(port)
 console.log('Running on port ' + port)
 
-export {app}
+export { app }
 
 
 
